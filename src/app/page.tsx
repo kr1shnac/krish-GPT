16f8dd575c003377ba39e5822aca106ac0f15a01
Krishna C <krishnacgowda10@gmail.com>
2026-02-12 19:45:03 +0530

    feat: add shadcn button component - trial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Krish-GPT</h1>
-      <p className="mt-4 text-xl">My AI Playground</p>
+      <h1 className="text-4xl font-bold">krish-GPT</h1>
+      <br />
+      <Button  size="lg">
+          f**k boy
+        </Button>
     </div>
   );
 }
