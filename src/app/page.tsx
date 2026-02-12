c853e8a9264bf35a0c5a9c1321e19f15bcdb5cb8
Krishna C <krishnacgowda10@gmail.com>
2026-02-12 23:11:32 +0530

    feat: add shadcn card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Field } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Card,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">krish-GPT</h1>
-      <br />
-      <Button  size="lg">
-          f**k boy
-        </Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <h1 className="text-4xl font-bold">krish-GPT</h1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>
+            <Field orientation="horizontal">
+              <Input type="search" placeholder="Search..." />
+              <Button>Search</Button>
+            </Field>
+          </p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
